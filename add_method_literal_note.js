11c599f1f1f5aea8bf10f2b7e766e7657d9040e1
Eli De Leon <elisandro.deleon@gmail.com>
2016-08-10 07:12:24 -0700

    first commit

---
+++ add_method_literal_note.js
@@ -0,0 +1,11 @@
+var james = {
+    job: "programmer",
+    married: false,
+    speak: function(string) {
+        console.log("Hello, I am feeling " + string);
+        
+    }
+};
+
+james.speak("great");
+james.speak("just okay");
